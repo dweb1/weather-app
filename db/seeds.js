0bd6f85b38119c26f6a39b52ed58e393c37c9a7d
dweb1 <david.p.weber@gmail.com>
2017-10-10 17:55:10 -0400

    added data models to database

---
+++ db/seeds.js
@@ -0,0 +1,24 @@
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/express-mongoose-lesson-starter');
+
+var User = require('../models/user');
+var City = require('../models/city');
+
+City.remove({}, function(err){
+    console.log(err);
+  });
+  
+User.remove({}, function(err){
+    console.log(err);
+});
+
+var atlanta = new City({
+    name: "Atlanta"
+})
+
+atlanta.save( (err) => {
+    if (err) console.log(err);
+    console.log('Atlanta created');
+})
+
+mongoose.connection.close();
---
+++ models/city.js
@@ -0,0 +1,5 @@
+var Schema = require("../db/schema");
+var mongoose = require('mongoose');
+
+var City = Schema.City;
+module.exports = City;
