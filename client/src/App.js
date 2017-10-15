96521290cf9aca504201e29193bb9183a61f5464
dweb1 <david.p.weber@gmail.com>
2017-10-15 12:52:13 -0400

    building call to weather API in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,26 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
+import axios from 'axios'
+
 
 import Header from './components/header'
 
 class App extends Component {
+
+  constructor(){
+    super();
+    this.state = {
+      user: {},
+      favoriteCities: []
+    }
+  }
+
+  fetchCities = () => {
+    // api.openweathermap.org/data/2.5/weather?q=Atlanta
+    
+    
+  }
+
   render() {
     return (
       <div>
--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,19 @@
 import React from 'react';
 import styled from 'styled-components'
 
+const Nav = styled.div`
+    width: 100%;
+    background: black;
+    height: 50px;
+    color: white;
+    margin: auto 0
+`
+
 const Header = () => {
     return (
-        <div>
+        <Nav>
             Hello world
-        </div>
+        </Nav>
     );
 };
 
